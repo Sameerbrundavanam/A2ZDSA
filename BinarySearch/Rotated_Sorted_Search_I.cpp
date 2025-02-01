b58839010f434508ba57b71d75a26ffaf6ff2fc8
SameerBrundavanam <22a91a0514@aec.edu.in>
2025-02-02 01:00:04 +0530

    RotatedSortedI

---
+++ BinarySearch/Rotated_Sorted_Search_I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low = 0, high = arr.size() - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid] == target) return mid;
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target and target <= arr[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(arr[mid] <= target and target <= arr[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
