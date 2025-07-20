a4d0fd6632b3b3c91b37ada54016b724baee0791
SameerBrundavanam <22a91a0514@aec.edu.in>
2025-07-21 00:04:06 +0530

    Code Updated

--- SlidingWindow_TwoPointer/Conatiner_With_Most_Water.cpp
+++ SlidingWindow_TwoPointer/Conatiner_With_Most_Water.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1, maxAmount = 0;
-        while(left < right){
-            int area = Math.min(height[left],height[right]) * (right - left);
-            maxAmount = Math.max(maxAmount,area);
-            if(height[left] < height[right]){
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1, maxAmount = 0;
+        while (left < right) {
+            int area = min(height[left], height[right]) * (right - left);
+            maxAmount = max(maxAmount, area);
+            if (height[left] < height[right]) {
                 left++;
-            } else{
+            } else {
                 right--;
             }
         }
         return maxAmount;
     }
-}
+};
